495bab45648cb36fa99d83b092a817cbda2196f2
Lilian Petitpas <lilian.petitpas@outlook.com>
2018-02-16 19:53:42 +0100

    Mise en place du switch

--- scripts/menu_principal.sh
+++ scripts/menu_principal.sh
@@ -13,4 +13,28 @@ if ! [ $WT_EXIT_STATUS = 0 ]; then
     exit 0
 fi
 
+case "$WT_MENU_CHOICE" in
+    "Compiler")
+        WT_TITLE="Compiler"
+        WT_MSG="Compiler"
+        _msgbox
+    ;;
+    "Creer le systeme")
+        WT_TITLE="Creer le systeme"
+        WT_MSG="Creer le systeme"
+        _msgbox
+    ;;
+    "Sauvegarder")
+        WT_TITLE="Sauvegarder"
+        WT_MSG="Sauvagarder"
+        _msgbox
+    ;;
+    "Restaurer")
+        WT_TITLE="Restaurer"
+        WT_MSG="Restaurer"
+        _msgbox
+    ;;
+    *)
+    ;;
+esac
 
